6d86e86fed8e177cadf42085af7aa5f18611ac50
Artem Tumin <arttumin@gmail.com>
2014-06-06 23:29:49 +0400

    +

--- public/js/main.js
+++ public/js/main.js
@@ -33,8 +33,35 @@ var Carousel = (function(){
 	};
 })();
 
+var Logos = (function(){
+	var $artDir = $('.art-director');
+	var $uiDes = $('.ui-designer');
+	var $curr = $artDir;
+	var $last = $uiDes;
+
+	return {
+		init: function() {
+			setTimeout( function(){ $artDir.removeClass('hidden').addClass('flipInX'); }, 2500 );
+			setTimeout( function(){ $uiDes.removeClass('hidden').addClass('flipInX'); }, 5000);
+
+			var timer = setTimeout( function run() {
+				Logos.changeStates($uiDes);
+				timer = setTimeout( run, 5000 );
+			}, 7000);
+		},
+		changeStates: function(elem) {
+			if(elem.hasClass('flipInX')) {
+				elem.removeClass('flipInX').addClass('flipOutX');
+			} else {
+				elem.removeClass('flipOutX').addClass('flipInX');
+			}
+		}
+	};
+})();
+
 App.init();
 Carousel.init();
+Logos.init();
 
 $(window).resize(function(){
 	App.init();
